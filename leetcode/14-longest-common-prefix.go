51b85bffed3466fdc96d873b3a686ec27e4d62bf
doldoldol21 <haejehaeje@gmail.com>
2026-04-06 15:05:38 +0900

    leetcode: 14

---
+++ leetcode/14-longest-common-prefix.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+// https://leetcode.com/problems/longest-common-prefix/description/
+func longestCommonPrefix(strs []string) string {
+	lcp := strs[0]
+
+	for i := 1; i < len(strs); i++ {
+		j := min(len(lcp), len(strs[i]))
+		for ; j > 0; j-- {
+			if lcp[:j] == strs[i][:j] {
+				lcp = lcp[:j]
+				break
+			}
+		}
+		if lcp[:j] == "" {
+			return ""
+		}
+	}
+
+	return lcp
+}
+
+func main() {
+	fmt.Println(longestCommonPrefix([]string{"flight", "fliy", "fligon"}))
+}
